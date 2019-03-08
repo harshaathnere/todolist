92406651938f53dc96db3dd06b15fd8dceacae10
harshaathnere <harshaathnere@gmail.com>
2019-03-08 16:14:19 +0530

    My very first commit

---
+++ typing/dom.js
@@ -0,0 +1,20 @@
+// // alert('123');
+// //Examine the document object
+// // console.dir(document);
+// // alert('786');
+// // console.log(document.URL);
+// // console.log(document.title);
+// // document.title ='123';
+// // console.log(document.doctype);
+// console.log(document.head);
+// console.log(document.body);
+// console.log(document.all);
+// console.log(document.all[10]);
+// console.log(document.forms);
+// console.log(document.links);
+// // document.all[10].textContent = "Hello";
+// console.log(document.images);
+
+//GETELEMENT BY ID
+console.log(document.getElementById('header-title'));
+var headerTitle = document.getElementById('header-title');
